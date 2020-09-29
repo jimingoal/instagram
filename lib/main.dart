d9827fe528383608425324c26fdc212922f78be7
jimingoal <jimingoal@gmail.com>
2020-09-29 11:25:06 +0900

    MaterialApp위젯

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('cool app'),
+      ),
       body: Container(
-        ),
-      );
+        color: Colors.amber,
+      ),
+    );
   }
 }
