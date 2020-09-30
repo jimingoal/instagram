90b34c87caddf7c959424179a7c98df9552ee29e
jimingoal <jimingoal@gmail.com>
2020-10-01 00:02:43 +0900

    로딩위젯 추가

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -3,13 +3,26 @@ import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
   final index;
+  Size size;
 
   Post(this.index);
 
   @override
   Widget build(BuildContext context) {
+    if (size == null) size = MediaQuery.of(context).size;
+
     return CachedNetworkImage(
       imageUrl: 'https://picsum.photos/id/$index/200/300',
+      placeholder: (context, url) {
+        return Container(
+            height: size.height,
+            width: size.width,
+            child: Center(
+                child: SizedBox(
+                    height: 50.0,
+                    width: 50.0,
+                    child: CircularProgressIndicator())));
+      },
       imageBuilder: (context, imageProvider) {
         return AspectRatio(
           aspectRatio: 1,
