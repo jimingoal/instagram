f452e4c2470ee603c895b8c70c7254d90656b3ed
jimingoal <jimingoal@gmail.com>
2020-09-30 22:51:33 +0900

    material white color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants/colors.dart';
 
 import 'home_page.dart';
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      theme: ThemeData(primarySwatch: white),
     );
   }
 }
