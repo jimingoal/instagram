d6f32160c3fe1e8f0be634e4817b4d359f6d6d6b
jimingoal <jimingoal@gmail.com>
2020-09-30 21:05:43 +0900

    bottom navigation bar생성

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -7,15 +6,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<BottomNavigationBarItem> btmNavItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('')),
+    BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('')),
+    BottomNavigationBarItem(icon: Icon(Icons.add), title: Text('')),
+    BottomNavigationBarItem(icon: Icon(Icons.healing), title: Text('')),
+    BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('')),
+  ];
+
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('cool app'),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black87,
+        items: btmNavItems,
+        currentIndex: _selectedIndex,
+        onTap: _onBtmItemClick,
       ),
       body: Container(
         color: Colors.amber,
       ),
     );
+  }
+
+  void _onBtmItemClick(int index) {
+    setState(() {
+      print(index);
+      _selectedIndex = index;
+    });
   }
 }
